dc258dd18bdfdf51f08810f23f3fc5d75e7b7209
Matthew Harrison Laptop <matthew_s_harrison@yahoo.com>
2023-01-18 13:07:04 +0000

    added twitter and github sign in

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import TwitterProvider from 'next-auth/providers/twitter'
+import GithubProvider from 'next-auth/providers/github'
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
@@ -9,6 +11,14 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string
     }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID as string,
+      clientSecret: process.env.TWITTER_SECRET as string
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string
+    })
     // ...add more providers here
   ],
 }
